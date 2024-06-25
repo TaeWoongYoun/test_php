82a8bc7be250a3883a57177d5d24be6d64027f21
xodnd0507 <24457545yong@gmail.com>
2024-06-25 13:23:42 +0900

    test : Table desgin

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>notice</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <table>
+        <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Title</th>
+            <th class="date">Date</th>
+        </tr>
+        <tr>
+            <td>1</td>
+            <td>HTML</td>
+            <td>html is ...</td>
+            <td>2024-05-07</td>
+        </tr>
+        <tr>
+            <td>2</td>
+            <td>CSS</td>
+            <td>css is ...</td>
+            <td>2024-06-08</td>
+        </tr>
+        <tr>
+            <td>3</td>
+            <td>JS</td>
+            <td>js is ...</td>
+            <td>2024-04-05</td>
+        </tr>
+        <tr>
+            <td>4</td>
+            <td>PHP</td>
+            <td>php is ...</td>
+            <td>2024-03-02</td>
+        </tr>
+    </table>
+</body>
+</html>
