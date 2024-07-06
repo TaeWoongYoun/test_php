ef6d1351b403290a6bd2d1ac1782e83ce64ee744
xodnd0507 <24457545yong@gmail.com>
2024-07-06 22:12:59 +0900

    test : Setting php

---
+++ create.php
@@ -0,0 +1,33 @@
+<?php $conn = mysqli_connect('localhost', 'root', '', 'test1')?>
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>create</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <table>
+        <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Title</th>
+            <th class="date">Date</th>
+        </tr>
+        <?php
+            $sql = "SELECT * FROM notice";
+            $result = mysqli_query($conn, $sql);
+            while($row = mysqli_fetch_array($result)){
+                echo "
+                <tr>
+                    <td>{$row['id']}</td>
+                    <td>{$row['name']}</td>
+                    <td>{$row['title']}</td>
+                    <td>{$row['date']}</td>
+                </tr>";
+            }
+        ?>
+    </table>
+</body>
+</html>
